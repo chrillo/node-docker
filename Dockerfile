a413545ba41d2c596b84acc47a375df12c3075ed
Christoph Peter <peter@internetkultur.at>
2018-10-06 13:58:28 +0200

    feat(pm2) fixed build

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,9 @@ FROM base
 WORKDIR /usr/src
 ENV NODE_ENV="production"
 COPY --from=base /usr/src .
+# RUN pm2 install pm2-server-monit
 # Expose port 8080
-EXPOSE 3000
+EXPOSE 8080
 CMD ["pm2-runtime", "start", "process_prod.json"]
 
 
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
  // respond with "hello world" when a GET request is made to the homepage
 app.get('*', function (req, res) {
   res.json({
       status:'ok',
-      version:process.pid
+      pid:process.pid
   })
 })
 
