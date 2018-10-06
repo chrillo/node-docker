3847bbcd4df7b19a89193454f93d67c78d99d9e0
Christoph Peter <peter@internetkultur.at>
2018-10-06 21:22:49 +0200

    feat(app): added started at timetsamp

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import express from 'express'
 const app = express()
 
 const port = process.env.PORT || 3000
+const startedAt = new Date()
  // respond with "hello world" when a GET request is made to the homepage
 app.get('*', function (req, res) {
   res.json({
       status:'ok',
       port,
       pid:process.pid,
-      node_env: process.env.NODE_ENV
+      node_env: process.env.NODE_ENV,
+      startedAt
   })
 })
 
